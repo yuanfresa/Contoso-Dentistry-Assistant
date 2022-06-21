251881b85d5ab24dfc55713ebe7c8c63b32ec738
Yuan Zhou <407246165@qq.com>
2022-06-21 22:28:31 +0200

    update the package.json

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 const path = require('path');
 
 const dotenv = require('dotenv');
@@ -9,53 +12,44 @@ const restify = require('restify');
 
 // Import required bot services.
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
-const {
-  ConfigurationServiceClientCredentialFactory,
-  createBotFrameworkAuthenticationFromConfiguration,
-  CloudAdapter
-} = require('botbuilder');
+const { BotFrameworkAdapter } = require('botbuilder');
 
 // This bot's main dialog.
 const { DentaBot } = require('./bot');
 
 // Create HTTP server
 const server = restify.createServer();
-server.use(restify.plugins.bodyParser());
 server.listen(process.env.port || process.env.PORT || 3978, () => {
-  console.log(`\n${server.name} listening to ${server.url}`);
-  console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
-  console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
 });
-
-const credentialsFactory = new ConfigurationServiceClientCredentialFactory({
-  MicrosoftAppId: process.env.MICROSOFTAPPID,
-  MicrosoftAppPassword: process.env.MICROSOFTAPPPASSWORD,
-});
-
-const botFrameworkAuthentication = createBotFrameworkAuthenticationFromConfiguration(null, credentialsFactory);
 
 // Create adapter.
 // See https://aka.ms/about-bot-adapter to learn more about how bots work.
-const adapter = new CloudAdapter(botFrameworkAuthentication);
+const adapter = new BotFrameworkAdapter({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
 
 // Catch-all for errors.
 const onTurnErrorHandler = async (context, error) => {
-  // This check writes out errors to console log .vs. app insights.
-  // NOTE: In production environment, you should consider logging this to Azure
-  //       application insights.
-  console.error(`\n [onTurnError] unhandled error: ${error}`);
+    // This check writes out errors to console log .vs. app insights.
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
+    console.error(`\n [onTurnError] unhandled error: ${error}`);
 
-  // Send a trace activity, which will be displayed in Bot Framework Emulator
-  await context.sendTraceActivity(
-    'OnTurnError Trace',
-    `${error}`,
-    'https://www.botframework.com/schemas/error',
-    'TurnError'
-  );
+    // Send a trace activity, which will be displayed in Bot Framework Emulator
+    await context.sendTraceActivity(
+        'OnTurnError Trace',
+        `${error}`,
+        'https://www.botframework.com/schemas/error',
+        'TurnError'
+    );
 
-  // Send a message to the user
-  await context.sendActivity('The bot encountered an error or bug.');
-  await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+    // Send a message to the user
+    await context.sendActivity('The bot encountered an error or bug.');
+    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
 };
 
 // Set the onTurnError for the singleton BotFrameworkAdapter.
@@ -63,48 +57,51 @@ adapter.onTurnError = onTurnErrorHandler;
 
 // Map configuration values values from .env file into the required format for each service.
 const QnAConfiguration = {
-  knowledgeBaseId: process.env.QNAKNOWLEDGEBASEID,
-  endpointKey: process.env.QNAAUTHKEY,
-  host: process.env.QNAENDPOINTHOSTNAME
+    knowledgeBaseId: process.env.QnAKnowledgebaseId,
+    endpointKey: process.env.QnAAuthKey,
+    host: process.env.QnAEndpointHostName
 };
 
 const LuisConfiguration = {
-  applicationId: process.env.LUISAPPID,
-  endpointKey: process.env.LUISAPIKEY,
-  endpoint: process.env.LUISAPIHOSTNAME,
+    applicationId: process.env.LuisAppId,
+    endpointKey: process.env.LuisAPIKey,
+    endpoint: process.env.LuisAPIHostName,
 }
 
 const SchedulerConfiguration = {
-  SchedulerEndpoint: process.env.SCHEDULERENDPOINT
+    SchedulerEndpoint: process.env.SchedulerEndpoint
 }
 //pack each service configuration into 
 const configuration = {
-  QnAConfiguration,
-  LuisConfiguration,
-  SchedulerConfiguration
+    QnAConfiguration,
+    LuisConfiguration,
+    SchedulerConfiguration
 }
 
 // Create the main dialog.
 const myBot = new DentaBot(configuration, {});
 
 // Listen for incoming requests.
-server.post('/api/messages', async (req, res) => {
-  // Route received a request to adapter for processing
-  await adapter.process(req, res, (context) => myBot.run(context));
-});
-
-server.get('/', (req, res) => {
-  res.json({
-    'message': 'Hello world!',
-  })
+server.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async (context) => {
+        // Route to main dialog.
+        await myBot.run(context);
+    });
 });
 
 // Listen for Upgrade requests for Streaming.
-server.on('upgrade', async (req, socket, head) => {
-  // Create an adapter scoped to this WebSocket connection to allow storing session data.
-  const streamingAdapter = new CloudAdapter(botFrameworkAuthentication);
-  // Set onTurnError for the CloudAdapter created for each connection.
-  streamingAdapter.onTurnError = onTurnErrorHandler;
+server.on('upgrade', (req, socket, head) => {
+    // Create an adapter scoped to this WebSocket connection to allow storing session data.
+    const streamingAdapter = new BotFrameworkAdapter({
+        appId: process.env.MicrosoftAppId,
+        appPassword: process.env.MicrosoftAppPassword
+    });
+    // Set onTurnError for the BotFrameworkAdapter created for each connection.
+    streamingAdapter.onTurnError = onTurnErrorHandler;
 
-  await streamingAdapter.process(req, socket, head, (context) => myBot.run(context));
+    streamingAdapter.useWebSocket(req, socket, head, async (context) => {
+        // After connecting via WebSocket, run this logic for every request sent over
+        // the WebSocket connection.
+        await myBot.run(context);
+    });
 });
